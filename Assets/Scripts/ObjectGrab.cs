fe4e817dca0110454a117c457479d190bdddcf6a
Lorelei Wright <wrighl@rpi.edu>
2018-11-04 18:41:53 -0500

    physics object pickup

---
+++ Assets/Scripts/ObjectGrab.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGrab : MonoBehaviour 
+{
+	[SerializeField] Transform m_LookTransform;
+	[SerializeField] Transform m_PickupSlot;
+	[SerializeField] float m_PickupDistance = 1f;
+
+	Pickup m_Object;
+	RaycastHit m_Hit;
+	bool m_Holding;
+
+	void Start() 
+	{
+		m_Holding = false;
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			bool DidRaycast = Physics.Raycast(m_LookTransform.position, 
+				m_LookTransform.forward, 
+				out m_Hit, 
+				m_PickupDistance,
+				(m_Holding) ? 1 << 9 : 1 << 11);
+			if (DidRaycast && m_Holding)
+			{
+				// I hit a machine
+				
+			}
+			else if (DidRaycast && !m_Holding)
+			{
+				// I hit a pickup
+				m_Object = m_Hit.transform.GetComponent<Pickup>();
+				m_Object.Grab(m_PickupSlot);
+				m_Holding = true;
+			}
+			else if (!DidRaycast && m_Holding)
+			{
+				// Drop the pickup
+				m_Object.Drop();
+				m_Holding = false;
+			}
+			else
+			{
+				// do nothing
+			}
+		}
+	}
+
+}
+ 
---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour 
+{
+	bool m_Grabbed;
+	Transform m_Target;
+	Rigidbody m_Rigidbody;
+
+	void Awake()
+	{
+		m_Grabbed = false;
+		m_Target = null;
+		m_Rigidbody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		if (m_Grabbed)
+		{
+			transform.position = Vector3.Lerp(transform.position, m_Target.position, 0.5f);
+		}
+	}
+
+	public void Grab(Transform t)
+	{
+		m_Target = t;
+		m_Rigidbody.isKinematic = true;
+		m_Grabbed = true;
+	}
+
+	public void Drop()
+	{
+		m_Grabbed = false;
+		m_Rigidbody.isKinematic = false;
+		m_Target = null;
+	}
+	
+}
