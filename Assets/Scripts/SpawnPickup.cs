9d50c11a3fea8d2105773a6690db9928ab11b929
Lorelei Wright <wrighl@rpi.edu>
2018-11-05 19:46:28 -0500

    Added pickup spawner

---
+++ Assets/Scripts/SpawnPickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPickup : MonoBehaviour 
+{
+	[SerializeField] GameObject m_Pickup;
+	[SerializeField] float m_InteractDistance;
+
+	ObjectGrab m_PlayerGrab;
+	Transform m_Player;
+
+	void Start()
+	{
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		m_PlayerGrab = player.GetComponent<ObjectGrab>();
+		m_Player = player.transform.GetChild(0);
+	}
+
+	void OnMouseDown()
+	{
+		if (Vector3.Distance(m_Player.position, transform.position) <= m_InteractDistance)
+		{
+			GameObject p = Instantiate(m_Pickup, transform.position + (0.7f * Vector3.up), Quaternion.identity);
+			m_PlayerGrab.Grab(p);
+		}
+	}
+
+	[ContextMenu("Spawn Pickup")]
+	void DoSpawnPickup()
+	{
+		Debug.Log("SPAWN");
+	}
+	
+}
